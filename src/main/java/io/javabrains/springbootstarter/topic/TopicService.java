37889335e51f5b10481f52a4b3ca1b7afdcbf456
Andy Atkinson <andyatkinson@gmail.com>
2017-08-14 15:16:49 -0500

    Add topics to a mutable array

--- src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
+++ src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
@@ -1,5 +1,6 @@
 package io.javabrains.springbootstarter.topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,11 +9,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class TopicService {
 	
-	private List<Topic> topics = Arrays.asList(
+	//we need a mutable array
+	private List<Topic> topics = new ArrayList<>(Arrays.asList(
 			new Topic("spring", "Spring Framework", "desc"),
 			new Topic("java", "Core Java", "desc2"),
 			new Topic("javascript", "JavaScript", "desc3")
-			);
+			));
 	
 	public List<Topic> getAllTopics() {
 		return topics;
@@ -21,6 +23,10 @@ public class TopicService {
 	public Topic getTopic(String id) {
 		// streams and lambda expression
 		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+	
+	public void addTopic(Topic topic) {
+		topics.add(topic);
 	}
 
 }
